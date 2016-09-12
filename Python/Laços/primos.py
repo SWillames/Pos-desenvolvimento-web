e85eea70d135457d63a8faf6c00fd48ac7aef05d
SWillames <sergiowoc@gmail.com>
2016-09-12 09:50:53 -0300

    Uma forma ainda melhor.

--- Python/Laços/primos.py
+++ Python/Laços/primos.py
@@ -33,5 +33,25 @@ for numero in xrange(2, limite+1):
         p1 += 1
 print "\n\nForam encontrados %d números primos." %p1
 print "Foram necessárias %d comparações, %d a menos quando feita com logica usada no while." % (c1, c-c1)
+print "\n"
+print "==========EXECUÇÃO AINDA MELHOR============"
+print "Primos: 2",
 
-#print "==========EXECUÇÃO AINDA MELHOR============"
+c2, p2, primos, limite = 1, 1, [2,], 1000
+
+for numero in xrange(3,limite+1,2): #pula de dois em dois
+    ehprimo = 1
+    for i in primos:
+        c += 1 # mudei de lugar
+        if numero % i == 0:
+            ehprimo = 0
+            break
+        if i > sqrt(numero):
+            break
+    if (ehprimo):
+        primos.append(numero)
+        print numero,
+        p += 1
+
+print "\n\nForam encontrados %d números primos." %p2
+print "Foram necessárias %d comparações, %d a menos quando feita com logica usada no while." % (c2, c1-c2)
