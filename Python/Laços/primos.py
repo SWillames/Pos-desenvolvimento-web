1d00c5dd9aadb06dd7d42cd424445b25fdf6532b
SWillames <sergiowoc@gmail.com>
2016-09-12 08:42:49 -0300

    Melhorando a execução do exemplo.

--- Python/Laços/primos.py
+++ Python/Laços/primos.py
@@ -17,3 +17,18 @@ while numero < 1000:
 
 print "\n\nForam encontrados %d números primos." %p
 print "Foram necessárias %d comparações." %c
+print "\n"
+
+print "==========EXECUÇÃO MELHORADA============"
+c1 = 1
+p1 = 0
+print "Primos: ",
+for numero in xrange(2, limite+1):
+    for i in xrange(2,numero):
+        if numero % i == 0: break
+        c1 += 1
+    else:
+        print numero,
+        p1 += 1
+print "\n\nForam encontrados %d números primos." %p1
+print "Foram necessárias %d comparações, %d a menos quando feita com logica usada no while." % (c1, c-c1)
