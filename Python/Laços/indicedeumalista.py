9d7de5ecc637c74d2c25c02cf5c4c12e9abed65a
SWillames <sergiowoc@gmail.com>
2016-09-13 01:12:58 -0300

    Exemplo usando for com indices.

---
+++ Python/Laços/indicedeumalista.py
@@ -0,0 +1,7 @@
+#coding: utf-8
+a = ['João', 'Rafael', 'Douglas', 'Pedro', 'Marcos', 'Sérgio']
+for i in a:
+    print i
+print "\n Imprimindo os indices!!"
+for j in range(len(a)):
+    print j, a[j]
