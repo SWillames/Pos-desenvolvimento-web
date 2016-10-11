94bd9f0a30cef2043ee4ce0c95ecf354e678ffee
SWillames <sergiowoc@gmail.com>
2016-10-11 16:49:01 -0300

    removendo chaves do dicionario criado.

--- Python/dicionarios/exemplo1.py
+++ Python/dicionarios/exemplo1.py
@@ -1,6 +1,10 @@
 #coding: utf-8
 aurelio = {'denominacao':'ilha solteira', 'populacao':23000, 'renda': 1500}
 print aurelio
+#ADICIONANDO NOVA CHAVE NO DICIONARIO
 aurelio['vocacao'] = 'turismo'
 print "Essa é o dicionario com uma nova caracteristica"
+print aurelio
+#REMOVENDO CHAVE DO DICIONARIO
+del aurelio['denominacao']
 print aurelio
