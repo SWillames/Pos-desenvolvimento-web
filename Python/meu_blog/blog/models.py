e8d2daca04ddfa6800c0b221f6df6a0e017b6687
SWillames <sergiowoc@gmail.com>
2017-05-05 12:40:02 -0300

    finalizando

---
+++ Python/meu_blog/blog/models.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Artigo(models.Model):
+    titulo = models.CharField(max_length=100)
+    conteudo = models.TextField()
+    publicacao = models.DateTimeField()
---
+++ Python/virtualenv/estude[py]/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/sergio/projetos/Pos-desenvolvimento-web/Python/virtualenv/estude[py]/bin/python2
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
